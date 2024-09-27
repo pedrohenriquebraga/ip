48fdbee26aa7f1af85e21011e54dd281b0a0b651
pedrohenriquebraga <gamesantos7@gmail.com>
2024-09-26 23:32:09 -0300

    Add 'Metodo Babilonico'

---
+++ exams/metodoBabilonico.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+
+  int num, i;
+  double raiz, x0;
+
+  scanf("%d", &num);
+
+  x0 = num / 2.0;
+
+  for (i = 0; i < 32; i++) {
+    if (i == 0) {
+      raiz = (1 / 2.0) * (x0 + (num / x0));
+    } else {
+      raiz = (1 / 2.0) * (raiz + (num / raiz));
+    }
+  }
+
+  printf("%.10lf\n", raiz);
+
+  return 0;
+}
