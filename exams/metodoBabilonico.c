9938ecda367f764df40cf9a6f3e2bc08665706e3
pedrohenriquebraga <gamesantos7@gmail.com>
2024-09-26 23:34:16 -0300

    simplify code

--- exams/metodoBabilonico.c
+++ exams/metodoBabilonico.c
@@ -8,13 +8,10 @@ int main() {
   scanf("%d", &num);
 
   x0 = num / 2.0;
+  raiz = (1 / 2.0) * (x0 + (num / x0));
 
   for (i = 0; i < 32; i++) {
-    if (i == 0) {
-      raiz = (1 / 2.0) * (x0 + (num / x0));
-    } else {
-      raiz = (1 / 2.0) * (raiz + (num / raiz));
-    }
+    raiz = (1 / 2.0) * (raiz + (num / raiz));
   }
 
   printf("%.10lf\n", raiz);
