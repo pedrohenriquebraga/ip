aa48ac897c5f7d0827c11cf7bba6aaecd12aeb4a
pedrohenriquebraga <gamesantos7@gmail.com>
2024-10-18 10:07:37 -0300

    Add p07

--- problems/lista_03/p07.c
+++ problems/lista_03/p07.c
@@ -1,6 +1,45 @@
 #include <stdio.h>
 
-int main() {
+void print_byte( unsigned char byte ) {
+	int peso = 128;
+	while(peso > 0) {
+		printf("%d", byte / peso);
+		byte %= peso;
+		peso /= 2;
+	}
+}
+void print_BYTES( void * ptr, int nbytes ) {
+	char * pc = ptr;
+	int i;
+	for(i = 0 ; i < nbytes ; i++) {
+		print_byte(*pc);
+		pc = pc + 1;
+		printf(" ");
+	}
+	
+}
 
-  return 0;
+
+int main() {
+	double x;
+	unsigned char ch;
+	unsigned short sh;
+	unsigned int ui;
+	float fl;
+
+	scanf("%lf", &x);
+
+	ch = sh = ui = fl = x;
+
+	print_BYTES( &ch, sizeof(char) );
+	printf("\n");
+	print_BYTES( &sh, sizeof(short) );
+	printf("\n");
+	print_BYTES( &ui, sizeof(int) );
+	printf("\n");
+	print_BYTES( &fl, sizeof(float) );
+	printf("\n");
+	print_BYTES( &x, sizeof(double) );
+	printf("\n");	
+	return 0;
 }
